9b49af1022213c6a8d3af22fd75f5b157d6784e1
irzelindo <irzelindo.salvador@gmail.com>
2022-01-12 23:59:39 +0200

    Add routes & json compability to main server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database';
+import { router } from "./routes"
 
 // Add typo libraries, from @types/<library>
 // @types/express - yarn add @types/express
 
 const app = express();
+
+app.use(express.json());
+
+app.use(router);
 
 const port = 3000
 
