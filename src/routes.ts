3f86fd53d630919dd6b555e921d0195b62baa167
irzelindo <irzelindo.salvador@gmail.com>
2022-01-18 22:35:01 +0200

    Add create tag route with admin verification

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/createUserController";
+import { CreateTagController } from "./controllers/createTagConroller";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
-const createUserController = new CreateUserController();
+const createUserController = new CreateUserController(); // Instace of create user controller
 
+const createTagConroller = new CreateTagController(); // Instance of create tag controller
+
+router.use(ensureAdmin) // middleware for authentication and authorization privileges
+router.post("/tags", ensureAdmin, createTagConroller.handle);
 router.post("/users", createUserController.handle);
 
 export { router };
