0130a83b0b344fb0398e7d99eece8fccf6e086cf
irzelindo <irzelindo.salvador@gmail.com>
2022-01-23 15:05:10 +0200

    Add session route for authentication

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/createUserController";
-import { CreateTagController } from "./controllers/createTagConroller";
+import { CreateTagController } from "./controllers/createTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 
 const router = Router();
 
@@ -9,8 +10,11 @@ const createUserController = new CreateUserController(); // Instace of create us
 
 const createTagConroller = new CreateTagController(); // Instance of create tag controller
 
+const authenticateUserController = new AuthenticateUserController(); // Instance of authentication user controller
+
 router.use(ensureAdmin) // middleware for authentication and authorization privileges
 router.post("/tags", ensureAdmin, createTagConroller.handle);
 router.post("/users", createUserController.handle);
+router.post("/sessions", authenticateUserController.handle);
 
 export { router };
