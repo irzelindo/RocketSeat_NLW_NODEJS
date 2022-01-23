6232feda7edca98b280882b4814980c17c86f713
irzelindo <irzelindo.salvador@gmail.com>
2022-01-23 15:03:27 +0200

    Add createTagController without typo error

---
+++ src/controllers/createTagController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { CreateTagService } from "../services/createTagService";
+
+/**
+ * Controller comunicates with the Service to handle de repositories functions
+ * Comunicates with the route providing a handle function for Request and Response
+ * The Service instance has the execute function with will execute the request on the
+ * Database using Entities 
+ */
+
+class CreateTagController {
+
+   async handle(request: Request, response: Response){
+
+        const createTagService = new CreateTagService();
+
+        const { name } = request.body
+
+        const tag = await createTagService.execute({ name });
+
+        return response.json(tag);
+   }
+}
+
+export { CreateTagController };
