04fdb8369c53b3c97cf48248139d55e0805e4ae0
irzelindo <irzelindo.salvador@gmail.com>
2022-02-28 19:11:51 +0200

    Update ensure Administartor rigths middleware

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,13 +1,27 @@
 import { Request, Response, NextFunction } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
-export function ensureAdmin(request: Request, response: Response, next: NextFunction) {
-    const admin = true;
+export async function ensureAdmin(
+    request: Request, 
+    response: Response, 
+    next: NextFunction) {
+
+    const { user_id } = request;
+    
+    // console.log(user_id);
+
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    const user = await usersRepositories.findOne(user_id);
+
+    const { admin } = user;
 
     if (admin) {
         return next();
     }
 
     return response.status(401).json({
-        error: "Unauthorized"
+        error: "Unauthorized - user is not admin"
     });
 }
